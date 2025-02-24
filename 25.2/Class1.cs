c776fd0936d02e7cb9760149c900e9911a28a5dc
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-24 20:38:19 +0200

    Add files via upload

---
+++ 25.2/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._2
+{
+    internal class  Software
+    {
+        public string name;
+        public string manufacturer;
+        public bool paid;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    name = value; 
+                else
+                    Console.WriteLine("Введите название");
+            }
+        }
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    manufacturer = value; 
+                else
+                    Console.WriteLine("Неизвестный производитель");
+            }
+        }
+
+        public bool Paid
+        {
+            get { return paid; }
+            set { paid = value; }
+        }
+
+        public Software()
+        {
+            name = "Неизвестно";
+            manufacturer = "Неизвестно";
+            paid = false;
+        }
+
+        public Software(string name, string manufacturer, bool isPaid)
+        {
+            this.name = name;
+            this.manufacturer = manufacturer;
+            this.paid = isPaid;
+        }
+
+        public override string ToString()
+        {
+            string type = paid ? "Платное" : "Бесплатное";
+            return $"Название: {name}, Производитель: {manufacturer}, Тип: {type}";
+        }
+
+        public void PaidStatus()
+        {
+            paid = !paid;
+        }
+
+        public bool IsFreeSoft(string targetManufacturer)
+        {
+            return !paid && manufacturer.Equals(targetManufacturer, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ 25.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Software software1 = new Software("Windows", "Microsoft", true);
+            Console.WriteLine(software1.ToString());
+            software1.PaidStatus();
+            Console.WriteLine("После изменения статуса:");
+            Console.WriteLine(software1.ToString());
+
+            Software software2 = new Software();
+            software2.name = "Linux";
+            software2.manufacturer = "Open Source";
+            software2.paid = false;
+            Console.WriteLine(software2.ToString());
+
+            Software software3 = new Software("Photoshop", "Adobe", true);
+            Console.WriteLine(software3.ToString());
+
+            Console.WriteLine("Введите название ПО:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите производителя:");
+            string manufacturer = Console.ReadLine();
+            Console.WriteLine("Платное ли ПО? (true/false):");
+            bool isPaid = bool.Parse(Console.ReadLine());
+            Software software4 = new Software(name, manufacturer, isPaid);
+            Console.WriteLine(software4.ToString());
+
+            Console.WriteLine("Является ли Linux бесплатным продуктом Open Source? " + software2.IsFreeSoft("Open Source"));
+        
+
+        Console.ReadKey();
+        }
+    }
+}
