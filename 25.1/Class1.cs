a6ca3eb01da16704d7759d18a86cf59847b21e1a
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-24 20:37:14 +0200

    Add files via upload

---
+++ 25.1/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._1
+{
+    internal class Edition
+    {
+
+        string name;
+        string surname;
+        int year;
+
+        public string Name
+        {
+            get { return (name != "") ? name : "неизвестно"; }
+            set => name = value;
+        }
+        public string Surname
+        {
+            get { return (surname != "") ? surname : "неизвестно"; }
+            set => surname = value;
+        }
+        public int Year
+        {
+            get { return (year != 0) ? year : 0; }
+            set
+            {
+                if (value < 1990)
+                {
+                    year = 1990;
+                }
+                else if (value > 2024)
+                {
+                    year = 2024;
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+        }
+
+        public Edition() { }
+
+        public Edition(string name, string surname, int year)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Year = year;
+        }
+
+        public int ChangePublicationYearRange(int year)
+        {
+            if (year > 1990 && year < 2024)
+            {
+                return year;
+            }
+            else
+            {
+                year = 2000;
+            }
+            return year;
+        }
+
+        public string FallIntoYear(int year)
+        {
+            if (year > 1990 && year < 2024)
+            {
+                return "Попадает в интервал";
+            }
+            else
+            {
+                return "Не попадает в интервал";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Surname = {Surname}, Year = {Year}";
+        }
+    }
+}
---
+++ 25.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Edition edition = new Edition();
+            Console.WriteLine(edition.ToString());
+            
+            Edition edition2 = new Edition("Лев", "Николаевич", 1908);
+            Console.WriteLine(edition2.ToString());
+           
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите Фамилию");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Введите год издания");
+            int year = int.Parse(Console.ReadLine());
+            Edition edition3 = new Edition(name, surname, year);
+            Console.WriteLine($"{edition3.ToString()}\n{edition3.ChangePublicationYearRange(year)}\n{edition3.FallIntoYear(year)}");
+        
+        }
+    }
+}
