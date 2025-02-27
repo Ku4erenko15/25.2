df3a0019ac25db17559b6243651a9c0bac970452
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-27 22:33:08 +0200

    Add files via upload

---
+++ 25.3/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._3
+{
+    internal class CBookCard
+    {
+        private string author;
+        private string title;
+        private string publisher;
+        private int yearOfPublication;
+        private int pageCount;
+        private int circulation;
+        private int rating;
+        private string comment;
+
+        public CBookCard(string author, string title, string publisher, int yearOfPublication, int pageCount, int circulation)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.yearOfPublication = yearOfPublication;
+            this.pageCount = pageCount;
+            this.circulation = circulation;
+            this.rating = 0; 
+            this.comment = string.Empty; 
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        public int YearOfPublication
+        {
+            get { return yearOfPublication; }
+            set { yearOfPublication = value; }
+        }
+
+        public int PageCount
+        {
+            get { return pageCount; }
+            set { pageCount = value; }
+        }
+
+        public int Circulation
+        {
+            get { return circulation; }
+            set { circulation = value; }
+        }
+
+        public int Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value >= 0 && value <= 100) 
+                    rating = value;
+                else
+                    throw new ArgumentException("Рейтинг должен быть в диапазоне от 0 до 100.");
+            }
+        }
+
+        public string Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+
+        public string GetBookInfo()
+        {
+            return $"Автор: {author}\n" +
+                   $"Заглавие: {title}\n" +
+                   $"Издательство: {publisher}\n" +
+                   $"Год издания: {yearOfPublication}\n" +
+                   $"Число страниц: {pageCount}\n" +
+                   $"Тираж: {circulation} : Рейтинг: {rating}\n" +
+                   $"Комментарий: {comment}";
+        }
+
+    }
+}
---
+++ 25.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CBookCard book = new CBookCard("Автор", "Заглавие", "Издательство", 2022, 228, 2077);
+            book.Rating = 85;  
+            book.Comment = "Читать всегда надо"; 
+
+            Console.WriteLine(book.GetBookInfo());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
